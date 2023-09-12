0322c65522c4f6250e67c7b71afafee273f9631d
KSmart329 <kile_smart@yahoo.com>
2023-09-11 18:34:18 -0700

    Initial commt

---
+++ 02_03/start/target/generated-test-sources/test-annotations/Autocomplete.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class Autocomplete {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:/Users/kdsvi/Downloads/chromedriver-win64/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/autocomplete");
+
+        WebElement autocomplete = driver.findElement(By.id("autocomplete"));
+        autocomplete.sendKeys("425 South Grand View Street, Apt. 104");
+
+        driver.quit();
+    }
+}
